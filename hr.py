f76171c56795b26a9a6e0f103c94c18d3a72b06f
tgab <thadgabara@gmail.com>
2024-05-15 08:58:27 -0400

    Create hr.py
    test python file

---
+++ hr.py
@@ -0,0 +1,11 @@
+class PayrollSystem:
+    def calculate_payroll(self, employees):
+        print("Calculating Payroll")
+        print("===================")
+        for employee in employees:
+            print(f"Payroll for: {employee.id} - {employee.name}")
+            print(f"- Check amount: {employee.calculate_payroll()}")
+            if employee.address:
+                print("- Sent to:")
+                print(employee.address)
+            print("")
